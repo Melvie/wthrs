cd7462e766268fefea32605cdacd71c71d2193c1
Melvie <3878772+Melvie@users.noreply.github.com>
2020-10-14 19:57:19 -0700

    Topic better forecast display (#5)
    * adds emoji to weather description
    
    * minor updates to forecast display

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use wthrs::weather_opt::WeatherOpt;
 use anyhow::Error;
+use wthrs::weather_opt::WeatherOpt;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     WeatherOpt::parse_args().await?;
     Ok(())
-
 }
